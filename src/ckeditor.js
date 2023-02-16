e916e1520140a9aa1c4ef7eac9e0f1447a0fb72f
srengty <srengty@gmail.com>
2023-02-16 16:24:40 +0700

    add fonts

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -8,6 +8,7 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import AutoSave from '@ckeditor/ckeditor5-autosave/src/autosave';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
@@ -17,6 +18,10 @@ import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock";
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -41,6 +46,7 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
+	Alignment,
 	Autoformat,
 	AutoSave,
 	Bold,
@@ -51,6 +57,10 @@ ClassicEditor.builtinPlugins = [
 	CodeBlock,
 	CloudServices,
 	EasyImage,
+	FontFamily,
+	FontSize,
+	FontColor,
+	FontBackgroundColor,
 	Heading,
 	Image,
 	ImageCaption,
@@ -74,12 +84,17 @@ ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
+			'fontFamily',
+			'fontSize',
+			'fontColor',
+			'fontBackgroundColor',
 			'|',
 			'bold',
 			'italic',
 			'link',
 			'bulletedList',
 			'numberedList',
+			'alignment',
 			'|',
 			'outdent',
 			'indent',
